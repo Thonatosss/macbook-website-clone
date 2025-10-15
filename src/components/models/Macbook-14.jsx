1f0efec89f71bf6aa781fb7a8e3349332fbd6e5a
Mykola Romashenko <sven4369@gmail.com>
2025-10-15 20:38:02 +0300

    Implement the feature section

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -28,7 +28,7 @@ export default function MacBookModel14(props) {
                 }
             }
         })
-    }, [color, scene])
+    }, [color, scene]);
 
     return (
         <group {...props} dispose={null}>
